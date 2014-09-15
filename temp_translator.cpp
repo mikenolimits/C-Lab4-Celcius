1d7a1576541b702bce56d9eb3abec7b8da123db9
mikenolimits <mkantor100@qc.cuny.edu>
2014-09-15 16:05:10 -0400

    made temperature translator

---
+++ temp_translator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+double t;
+
+cout<<"what is the current temperature in celcius your room?";
+
+cin>>t;
+
+cout<<"The Fahrenheit Temperature is: "<<(t * 1.8)+ 32<<"degrees\n";
+
+
+return 0;
+
+}
