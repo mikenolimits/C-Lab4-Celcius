24194df49f300609de6ed3324178ff38524aa53e
mikenolimits <mkantor100@qc.cuny.edu>
2014-09-15 16:28:36 -0400

    added more options

--- temp_translator.cpp
+++ temp_translator.cpp
@@ -3,15 +3,37 @@ using namespace std;
 
 int main(){
 
-double t;
+    double amount; 
 
-cout<<"what is the current temperature in celcius your room?";
+    double t;
+    double w;
+    string unit;
+    string option;
 
-cin>>t;
+    cout<<"what would you like to translate?\n";
+    cin>>option;
 
-cout<<"The Fahrenheit Temperature is: "<<(t * 1.8)+ 32<<"degrees\n";
+    if(option == "temp")
+    cout<<"what is the current temperature in celcius your room?";
 
+    cin>>t;
 
-return 0;
+    cout<<"The Fahrenheit Temperature is: "<<(t * 1.8)+ 32<<"degrees\n";
+
+    if(option == "metric")
+
+    cout<<"what would you like to convert?\n";
+
+    cin>>unit;
+
+    cout<<"Great! How many ?" <<amount<<"s do you have?";
+
+    cin>>amount;
+
+    if(unit == "miles")
+
+    cout<<"You have : "<<amount/.621371<<"kilometers";
+
+    return 0;
 
 }
